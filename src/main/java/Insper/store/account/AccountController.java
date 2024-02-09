e72d6cd363f345a95427d362d1829175a92613ac
Eduardosmb <101536778+Eduardosmb@users.noreply.github.com>
2024-02-09 09:00:26 -0300

    ber3

--- src/main/java/Insper/store/account/AccountController.java
+++ src/main/java/Insper/store/account/AccountController.java
@@ -1,5 +1,16 @@
-public package Insper.store.account;
+package Insper.store.account;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 public interface AccountController{
-    public 
+
+    @PostMapping("/accounts")
+    public ResponseEntity<AccountOut> create(
+        @RequestBody(required = true) AccountIn in
+        );
+
+    @PutMapping("/accounts/{id}")
+    public ResponseEntity<AccountOut> update(string id, AccountIn accountIn);
 }
